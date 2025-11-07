7f0e1cd11b26f8a7dde98c7b8c6f575021ca90d9
19Suga <russelluna4321@gmail.com>
2025-11-06 23:42:05 -0400

    ultimo commit

---
+++ WebAPI1/WebAPI1/Models/DTOs/UpdateUserDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI1.Models.DTOs
+{
+    public class UpdateUserDto
+    {
+       
+        [Required]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string? UserName { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
